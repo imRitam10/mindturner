5865010bcf49ed977738e1a8720fd8d8b0be050b
Ritam Bhadra <imritam29@gmail.com>
2020-08-09 17:46:17 +0530

    db

--- app.js
+++ app.js
@@ -16,12 +16,17 @@ var express          = require("express"),
 
 
 // mongoose.connect("mongodb://localhost/mindturner");
-mongoose.connect('mongodb+srv://ritam:@BitTU2020@cluster0.vohou.mongodb.net/mindturner?retryWrites=true&w=majority' , {
-    useNewUrlParser: true,
-    useCreateIndex: true
-}).then(() => {
-    console.log ('ERROR:', err.message);
-});
+
+// mongoose.connect('mongodb+srv://ritam:@BitTU2020@cluster0.vohou.mongodb.net/mindturner?retryWrites=true&w=majority',{
+//     useNewUrlParser: true,
+//     useCreateIndex: true
+// }).then(() => {
+//     console.log ('ERROR:', err.message);
+// });
+
+mongoose.connect(process.env.DATABASEURL);
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -331,6 +336,4 @@ app.get("/index/:id/comments/:comment_id/edit",checkCommentOwnership, function(r
 
 
  
-// app.listen(process.env.PORT || 3000);
-
-app.listen(process.env.PORT, process.env.IP);
+app.listen(process.env.PORT || 3000);
