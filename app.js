17815ac63cb14dd2c5a34290beb8286e45f78f0a
Ritam Bhadra <imritam29@gmail.com>
2020-08-09 18:27:50 +0530

    DB

--- app.js
+++ app.js
@@ -17,14 +17,25 @@ var express          = require("express"),
 
 // mongoose.connect("mongodb://localhost/mindturner");
 
-mongoose.connect('mongodb+srv://ritam:@BitTU2020@cluster0.vohou.mongodb.net/mindturner?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useCreateIndex: true
-}).then(() => {
-    console.log ('ERROR:', err.message);
-});
+// mongoose.connect('mongodb+srv://ritam:@BitTU2020@cluster0.vohou.mongodb.net/mindturner?retryWrites=true&w=majority',{
+//     useNewUrlParser: true,
+//     useCreateIndex: true
+// }).then(() => {
+//     console.log ('ERROR:', err.message);
+// });
 
 // mongoose.connect(process.env.DATABASE);
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://ritam:@BitTU2020@cluster0.vohou.mongodb.net/mindturner?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
